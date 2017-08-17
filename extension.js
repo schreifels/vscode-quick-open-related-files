25b04bc3a3621dda0ed33b73766d0b264d0f40df
Mike Schreifels <mike@schreifels.org>
2017-08-16 18:02:25 -0700

    Add levelsToPreserve config option

--- extension.js
+++ extension.js
@@ -1,9 +1,14 @@
+const path = require('path');
 const vscode = require('vscode');
 
 function showRelatedFiles() {
+    const separator = path.sep;
+    const { levelsToPreserve } = vscode.workspace.getConfiguration('quickPickRelatedFiles');
+
     const { fileName } = vscode.window.activeTextEditor.document;
-    const fileNameParts = fileName.split('/');
-    const prefilledText = fileNameParts[fileNameParts.length - 1];
+    const fileNameParts = fileName.split(separator);
+    const prefilledText = fileNameParts.slice(fileNameParts.length - 1 - levelsToPreserve).join(separator);
+
     vscode.commands.executeCommand('workbench.action.quickOpen', prefilledText);
 }
 
