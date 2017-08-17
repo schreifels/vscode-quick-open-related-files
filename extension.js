b9c5cbd8cb2e11fb1c74f331245e4c3ee927812e
Mike Schreifels <mike@schreifels.org>
2017-08-16 17:50:48 -0700

    Use 4 spaces to be consistent with other files

--- extension.js
+++ extension.js
@@ -1,14 +1,14 @@
 const vscode = require('vscode');
 
 function showRelatedFiles() {
-  const { fileName } = vscode.window.activeTextEditor.document;
-  const fileNameParts = fileName.split('/');
-  const prefilledText = fileNameParts[fileNameParts.length - 1];
-  vscode.commands.executeCommand('workbench.action.quickOpen', prefilledText);
+    const { fileName } = vscode.window.activeTextEditor.document;
+    const fileNameParts = fileName.split('/');
+    const prefilledText = fileNameParts[fileNameParts.length - 1];
+    vscode.commands.executeCommand('workbench.action.quickOpen', prefilledText);
 }
 
 function activate(context) {
-  context.subscriptions.push(vscode.commands.registerCommand('quickPickRelatedFiles.show', showRelatedFiles));
+    context.subscriptions.push(vscode.commands.registerCommand('quickPickRelatedFiles.show', showRelatedFiles));
 }
 
 module.exports.activate = activate;
