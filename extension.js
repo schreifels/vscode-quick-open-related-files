2ac8051cea70d2023f866e22524445903103085a
Mike Schreifels <mike@schreifels.org>
2017-08-16 17:35:33 -0700

    Create basic proof of concept

--- extension.js
+++ extension.js
@@ -1,30 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function quickOpen() {
+  const { fileName } = vscode.window.activeTextEditor.document;
+  const fileNameParts = fileName.split('/');
+  const prefilledText = fileNameParts[fileNameParts.length - 1];
+  vscode.commands.executeCommand('workbench.action.quickOpen', prefilledText);
+}
+
 function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "quick-pick-related-files" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(vscode.commands.registerCommand('extension.sayHello', quickOpen));
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+module.exports.activate = activate;
