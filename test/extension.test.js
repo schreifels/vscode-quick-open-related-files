543f51ee034ce4692fbb4cbff520af94a06ed9c2
Mike Schreifels <mike@schreifels.org>
2017-08-18 22:36:20 -0700

    Add tests

--- test/extension.test.js
+++ test/extension.test.js
@@ -31,22 +31,27 @@ suite('extension tests', function() {
         assert.strictEqual(buildPrefix(2), 'app/models/person_spec.rb');
         assert.strictEqual(buildPrefix(6), 'Users/mike/projects/example-project/app/models/person_spec.rb');
         assert.strictEqual(buildPrefix(10), 'Users/mike/projects/example-project/app/models/person_spec.rb');
+
+        assert.strictEqual(buildPrefix(0, { currentFilename: '' }), '');
     });
 
     test('respects workspaceFolder', function() {
         assert.strictEqual(buildPrefix(0, { workspaceFolder: '/Users/mike/projects/example-project' }),
             'person_spec.rb');
+            assert.strictEqual(buildPrefix(2, { workspaceFolder: '/Users/mike/projects/example-project' }),
+            'app/models/person_spec.rb');
         assert.strictEqual(buildPrefix(10, { workspaceFolder: '/Users/mike/projects/example-project' }),
             'app/models/person_spec.rb');
-        assert.strictEqual(buildPrefix(6, { workspaceFolder: '/some/unrelated/path' }),
+        assert.strictEqual(buildPrefix(10, { workspaceFolder: '/some/unrelated/path' }),
             'Users/mike/projects/example-project/app/models/person_spec.rb');
-        assert.strictEqual(buildPrefix(6, { workspaceFolder: '/app/models' }),
+        assert.strictEqual(buildPrefix(10, { workspaceFolder: '/app/models' }),
             'Users/mike/projects/example-project/app/models/person_spec.rb');
     });
 
     test('respects patternsToStrip', function() {
         assert.strictEqual(buildPrefix(1, { currentFilename: '/dir/person.html.erb', patternsToStrip: [] }), 'dir/person.html.erb');
         assert.strictEqual(buildPrefix(1, { currentFilename: '/dir/person.html.erb', patternsToStrip: ['{EXTENSION}'] }), 'dir/person');
+        assert.strictEqual(buildPrefix(1, { currentFilename: '/dir/person.rb', patternsToStrip: ['{EXTENSION}'] }), 'dir/person');
         assert.strictEqual(buildPrefix(1, { currentFilename: '/dir/.gitignore', patternsToStrip: ['{EXTENSION}'] }), 'dir/.gitignore');
     });
 });
